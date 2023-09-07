e0e0bc4ee5c6500ff26ab0523e48d3a48cf027b7
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-09-06 19:00:09 -0500

    update server.js

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+//for security
+const helmet = require("helmet");
+
+dotenv.config();
+const PORT = process.env.PORT || 9090;
+const app = express();
+
+// Middlewae config
+app.use(helmet);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+//listen
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
