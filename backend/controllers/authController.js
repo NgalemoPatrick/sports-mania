0a9d359925d5fe73fb24137d3d2a1fcb5c1f2620
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-09-06 21:05:26 -0500

    add controllers folder

---
+++ backend/controllers/authController.js
@@ -0,0 +1,21 @@
+const Users = require("./models/userModel.js");
+
+export const register = async (req, res, next) => {
+  const { firstName, lastName, email, password } = req.body;
+  // check if the fileds are validate
+  if (!(firstName || lastName || email || password)) {
+    next("Provide Require Fields!");
+    return;
+  }
+  try {
+    const userExist = await Users.findOne({ email });
+
+    if (userExist) {
+      next("Email already exists");
+    }
+    const hashedPassword = await hashedPassword(password);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({});
+  }
+};
